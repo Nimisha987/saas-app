b5d075ad2707e449709929dc7336a779b6727799
Nimisha <nimishaagrawal115@gmail.com>
2026-01-06 22:57:24 +0530

    Implemented auth, database setup

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
+import { subjectsColors } from "@/constants"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const getSubjectColor = (
+  subject: keyof typeof subjectsColors
+) => {
+  return subjectsColors[subject];
+};
